76716764f7a34cf55ddfebe6e79233951bb7be82
Karmvir_Mandal <75217722+KarmvirMandal@users.noreply.github.com>
2020-12-01 01:07:26 +0530

    initial commit

---
+++ src/main/java/com/example/demo/OddEven.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class OddEven {
+	
+	public static void main(String args[])
+	  {
+	    int num;
+	    System.out.println("Enter an Integer number:");
+
+	    Scanner input = new Scanner(System.in);
+	    num = input.nextInt();
+	    
+	    if ( num % 2 == 0 )
+	        System.out.println("Entered number is even");
+	     else
+	        System.out.println("Entered number is odd");
+
+}
+}
